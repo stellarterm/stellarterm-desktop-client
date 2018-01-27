140ef85fd4b1fac13649249c3feb442877600556
Iris Li <github@iris.li>
2018-01-27 13:30:25 -0800

    Add release script

---
+++ buildRelease.sh
@@ -0,0 +1,15 @@
+# Builds the release notes
+
+ST_VERSION=$(egrep -o 'window.stBuildInfo={version:[0-9]+' index.html | egrep -o '[0-9]+')
+ST_VVERSION=v${ST_VERSION}
+# echo $ST_VERSION
+
+printf "StellarTerm Desktop Client v$ST_VERSION\n\n" > github-release-notes.txt
+
+printf '```\n' >> github-release-notes.txt
+npm run -s hash >> github-release-notes.txt
+printf '```\n' >> github-release-notes.txt
+
+cat github-release-notes.txt
+
+hub release create -d -f github-release-notes.txt -a release-builds/stellarterm-osx.zip -a release-builds/stellarterm-win32-x64.zip $ST_VVERSION
