9ca6a51cfde634dfa50dd6b9300bff8c342cccd4
Iris Li <github@iris.li>
2018-04-19 14:38:59 -0700

    SHA256(index.html)= d145cc3b3fff3c6e186f2323f95f16e886ca78da00cf4c8a24b79e68287a6b1e

--- fetchTravisBuild.sh
+++ fetchTravisBuild.sh
@@ -1,4 +1,4 @@
-# Usage: ./fetchTravisBuild.sh stellarterm-build-output/irisli/stellarterm/24/24.1/dist/index.html 3baf63b28afa7f95f34fe16f650b4e6454273c24090b2f0e94553ae7e001fbea
+# Usage: ./fetchTravisBuild.sh stellarterm-build-output/stellarterm/stellarterm/24/24.1/dist/index.html 3baf63b28afa7f95f34fe16f650b4e6454273c24090b2f0e94553ae7e001fbea
 aws s3 cp s3://$1 .  --region us-east-1
 HASH=$(openssl dgst -sha256 index.html | sed 's/^.* //')
 
