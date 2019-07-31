07d332d2cf819040b0ed8b17fae40c7820c32804
shiny-mountain <shiny-mountain@lobstr.co>
2019-07-31 15:31:20 +0300

    Added "stellar-protocol: SEP-0007" support

--- main.js
+++ main.js
@@ -1,108 +1,145 @@
-const electron = require('electron')
+const electron = require('electron');
 // Module to control application life.
-const app = electron.app
+const app = electron.app;
 // Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const BrowserWindow = electron.BrowserWindow;
 const opn = require('opn');
 
-const path = require('path')
-const url = require('url')
-var Menu = electron.Menu;
+const path = require('path');
+const url = require('url');
+let Menu = electron.Menu;
 
 require('./electron-context-menu-master/index.js')({
-  prepend: (params, browserWindow) => [{
-    visible: params.mediaType === 'input',
-  }]
+    prepend: (params, browserWindow) => [{
+        visible: params.mediaType === 'input',
+    }]
 });
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let mainWindow;
 
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    show: false,
-    webPreferences: {
-      nodeIntegration:false,
-    },
-    width: 1080,
-    minWidth: 1080,
-    height: 800,
-    minHeight: 600
-  })
+function createWindow() {
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        show: false,
+        webPreferences: {
+            nodeIntegration: false,
+        },
+        width: 1080,
+        minWidth: 1080,
+        height: 800,
+        minHeight: 600
+    });
 
-  mainWindow.maximize();
-  mainWindow.show();
+    mainWindow.maximize();
+    mainWindow.show();
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+    // and load the index.html of the app.
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true,
+        hash: link,
+    }));
 
-  mainWindow.webContents.on('will-navigate', function(event, url){
-    event.preventDefault();
-    opn(url);
-  });
 
-    var template = [{
-      label: "Application",
-      submenu: [
-          { label: "StellarTerm.com", click: function() { opn('https://stellarterm.com/'); } },
-          { type: "separator" },
-          { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
-      ]}, {
-      label: "Edit",
-      submenu: [
-          { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
-          { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
-          { type: "separator" },
-          { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
-          { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
-          { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
-          { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
-      ]}
-  ];
+    mainWindow.webContents.on('will-navigate', function (event, url) {
+        event.preventDefault();
+        opn(url);
+    });
 
-  // This menu isn't actually used. However, the hotkeys get used. This is how
-  // hotkeys for copy and paste are implemented.
-  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+    let template = [{
+        label: "Application",
+        submenu: [
+            {
+                label: "StellarTerm.com", click: function () {
+                    opn('https://stellarterm.com/');
+                }
+            },
+            {type: "separator"},
+            {
+                label: "Quit", accelerator: "Command+Q", click: function () {
+                    app.quit();
+                }
+            }
+        ]
+    }, {
+        label: "Edit",
+        submenu: [
+            {label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:"},
+            {label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:"},
+            {type: "separator"},
+            {label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:"},
+            {label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:"},
+            {label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:"},
+            {label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:"}
+        ]
+    }
+    ];
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+    // This menu isn't actually used. However, the hotkeys get used. This is how
+    // hotkeys for copy and paste are implemented.
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+    // Open the DevTools.
+    // mainWindow.webContents.openDevTools()
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function () {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding elem.
+        mainWindow = null
+    })
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+});
 
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow()
+    }
+});
+
+app.setAsDefaultProtocolClient('web+stellar');
+
+let link;
+
+app.on('open-url', (event, data) => {
+    event.preventDefault();
+    link = data;
+    if (mainWindow === null) {
+        createWindow();
+    }
+    if (app.isReady() && mainWindow) {
+        mainWindow.loadURL(url.format({
+            pathname: path.join(__dirname, 'index.html'),
+            protocol: 'file:',
+            slashes: true,
+            hash: link,
+        }));
+        if (mainWindow.isMinimized()) {
+            mainWindow.restore();
+        }
+        mainWindow.show();
+    }
+});
+
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
